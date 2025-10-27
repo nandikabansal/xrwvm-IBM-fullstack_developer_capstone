ebda970806e96f9d1e84a7bd7090d6343c883ed9
nandikabansal <nandikabansal16@gmail.com>
2025-10-27 16:40:33 -0400

    Redone task 3

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,12 +11,21 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
 
-    # path for dealer reviews view
+    # path for dealer reviews view (get_dealer_reviews)
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
 
     # path for add a review view
+    path(route='add_review', view=views.add_review, name='add_review'),
 
     #path for logout
-    path('logout', views.logout_request, name='logout')
+    path('logout', views.logout_request, name='logout'),
+
+    #path for get_dealerships
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+        path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+
+    #path for get_dealer_details
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
